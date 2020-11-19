9fd0ff77801fcbfa914aa5d7917faff2cdc7dd74
XFGuo <guoxiafei1991@163.com>
2020-11-19 15:59:20 +0800

    creat project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+/**
+* @Name: ContentView.swift
+* @Description:
+* @Author: guoxiafei
+* @Date: 2020/11/19
+* @Copyright: 
+*/
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,20 @@
+//
+/**
+* @Name: WeSplitApp.swift
+* @Description:
+* @Author: guoxiafei
+* @Date: 2020/11/19
+* @Copyright: 
+*/
+
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
